c50eebac2c38bdc97aae18b9e721d83222532f9d
susmitha-20120 <susmitharedddy2006@gmail.com>
2025-05-17 10:12:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
